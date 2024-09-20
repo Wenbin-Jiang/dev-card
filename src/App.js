e9f534c23ec9c2dc8e804c1f872b870183625dc0
wenbinJ <157052371+Wenbin-Jiang-Section-E@users.noreply.github.com>
2024-09-20 19:42:40 -0400

    small profile card project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data"></div>
+      <Intro />
+      <SkillList />
     </div>
   );
 }
 
+const Avatar = () => {
+  return <img className="avatar" src="avatar.jpeg" alt="Avatar" />;
+};
+
+const Intro = () => {
+  return (
+    <div className="data">
+      <h1>Wenbin Jiang</h1>
+      <p>
+        I'm a student studying Computer Programming at Humber Polytech. I'm also
+        a Dental Hygienist. I enjoy winter more than summer because I love
+        snowboarding.
+      </p>
+    </div>
+  );
+};
+
+const SkillList = () => {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="red" />
+      <Skill skill="Javascript" emoji="💪" color="yellow" />
+      <Skill skill="HTML+CSS" emoji="💪" color="pink" />
+      <Skill skill="Node.js" emoji="💪" color="green" />
+      <Skill skill="Python" emoji="💪" color="orange" />
+    </div>
+  );
+};
+
+const Skill = (props) => {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+};
 export default App;
